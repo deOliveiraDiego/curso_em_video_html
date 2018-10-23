2acc2226b84a1cb57db436ac4c4f8e6ae09d6c94
deOliveiraDiego <diego.oliveira.silva.07@gmail.com>
2018-10-23 18:32:04 -0300

    Aula 25

---
+++ _javascript/funcoes.js
@@ -0,0 +1,8 @@
+function mudaFoto (foto) {
+    document.getElementById('icone').src = foto;
+    oculos = '_imagens/glass-oculos-preto-peq.png'
+    specs = '_imagens/especificacoes.png'
+    fotos = '_imagens/fotos.png'
+    multi = '_imagens/multimidia.png'
+    contato = '_imagens/contato.png'
+}
